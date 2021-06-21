b085b8d679a7ed17b3fb7430b745376608e79922
Le0sy <leo.sooyeonlee@gmail.com>
2021-06-22 00:09:52 +0900

    On branch master  Changes to be committed: 	new file:   step5/_10818.java

---
+++ step5/_10818.java
@@ -0,0 +1,38 @@
+package step5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _10818 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int min = Integer.parseInt(st.nextToken());
+        int max = min;
+        int tmp;
+
+        while (st.hasMoreTokens()) {
+            tmp = Integer.parseInt(st.nextToken());
+            if (tmp < min) {
+                min = tmp;
+            } else if (tmp > max) {
+                max = tmp;
+            }
+        }
+        System.out.println(min + " " + max);
+        /*
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[N - 1]);
+        */
+    }
+}
